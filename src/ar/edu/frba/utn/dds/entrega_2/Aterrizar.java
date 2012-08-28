1646afe6f7f333f24a7c1a64039f76429b55742d
nuxlic <cangini.f@hotmail.com>
2012-08-28 15:05:47 -0300

    obtener itinerarios con o sin escala

--- src/ar/edu/frba/utn/dds/entrega_2/Aterrizar.java
+++ src/ar/edu/frba/utn/dds/entrega_2/Aterrizar.java
@@ -102,42 +102,7 @@ public class Aterrizar {
 				+ unUsuario.getTipo().getRecargo();
 	}
 
-	// public void obtenerItinerariosDeViaje(String unOrigen, String unDestino,
-	// Fecha fecha, Usuario user, Aerolinea unaAerolinea) {
-	// // Tengo que obtener todos los asientos disponibles con ese origen y
-	// // destino null
-	// // Tengo que obtener todos los asientos disponibles con ese destino y
-	// // origen null
-	// // tengo que fijarme por cada asiento de los origenes y por cada asiento
-	// // de los destinos si son o no iguales
-	// // en caso que sean iguales tengo que meterlos en el itinerario (no se
-	// // como hacerlos)
-	// // en caso quie NO sean iguales tengo que volver a llamar el metodo pero
-	// // con los destinos de los origenes y los origenes de los destinos.
-	// // por ahora pensamos en terminarlo con un contador. Sin embargo hay que
-	// // pensar como devolver la lista de itinerarios
-	// List<Asiento> asientosOrigenes =
-	// this.asientosDisponiblesDeUnaAerolinea(unOrigen,null, fecha, user,
-	// unaAerolinea);
-	// List<Asiento> asientosDestinos =
-	// this.asientosDisponiblesDeUnaAerolinea(null, unDestino, fecha, user,
-	// unaAerolinea);
-	// Itinerario unItinerario = new Itinerario();
-	//
-	// for(Asiento unAsientoOrigen: asientosOrigenes){
-	// for(Asiento unAsientoDestino: asientosDestinos){
-	// if(unAsientoOrigen.getDestino() != unAsientoDestino.getOrigen()){
-	// obtenerItinerariosDeViaje(unAsientoOrigen.getDestino(),
-	// unAsientoDestino.getOrigen(), fecha, user,
-	// unaAerolinea,asientosResultado);
-	// }
-	// unItinerario.getAsientos().add(unAsientoOrigen);
-	// unItinerario.getAsientos().add(unAsientoDestino);
-	// }
-	// //Aca tengo que cargar la lista de itinerarios.
-	// }
-	// }
-
+	
 	public List<Itinerario> obtenerItinerariosQueSatisfaganUnViajeDeUnaEscala(
 			String unOrigen, String unDestino, Fecha fecha, Usuario user,
 			Aerolinea unaAerolinea) {
@@ -162,9 +127,6 @@ public class Aterrizar {
 			}
 		}
 
-		// if(itinerarios.size()==0){
-		// throw new NoExistenEscalasPosiblesExeption();
-		// }
 
 		return itinerarios;
 
